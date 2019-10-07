db20caafb25185cd8b101c06d82da4e7c3241499
Nicolas Gordillo <nicolas.gordillo@axelra.com>
2019-10-07 17:17:09 +0200

    Always return constant value

--- src/core/InternalAnimatedValue.js
+++ src/core/InternalAnimatedValue.js
@@ -25,10 +25,11 @@ function initializeConstantValues() {
  */
 export default class InternalAnimatedValue extends AnimatedNode {
   static valueForConstant(number) {
-    initializeConstantValues();
+    /*initializeConstantValues();
     return (
       CONSTANT_VALUES.get(number) || new InternalAnimatedValue(number, true)
-    );
+    );*/
+    return new InternalAnimatedValue(number, true);
   }
 
   constructor(value, constant = false) {
